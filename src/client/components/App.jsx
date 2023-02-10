26327ff650876bc81404f6a07a62c2588beb9a2d
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-10 15:36:06 +1300

    create a route to the Home component

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Home from './Home'
+import Nav from './Nav'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+      <Nav />
+        <Routes>
+          <Route exact path='/' element={ <Home /> }>
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
