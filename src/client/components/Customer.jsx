b4d000c3ca4f57bda0391ca8c7b498265d8a833e
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-10 22:33:03 +1300

    move the edit and delete buttons to single customr

--- src/client/components/Customer.jsx
+++ src/client/components/Customer.jsx
@@ -10,14 +10,17 @@ export default function Customer () {
   
   return (
     <div>
-      <h2>Account Information</h2>
-      <p>Account Name: {customer.familyName}, {customer.givenName} {customer.middleName}</p>
-      <p>Address: {customer.address}</p>
-      <p>Mode: {customer.mode}</p>
-      <p>Balance: {customer.balance}</p>
-      <p>Due Date: {customer.dueDate}</p>
-      <p>Period: {customer.period}</p>
-      <p>Co-Maker: {customer.coMaker}</p>
+      <h2><strong>Personal Information</strong></h2>
+        <p>Account Name: {customer.familyName}, {customer.givenName} {customer.middleName}</p>
+        <p>Address: {customer.address}</p>
+      <h2><strong>Account Details</strong></h2>
+        <p>Mode: {customer.mode}</p>
+        <p>Balance: {customer.balance}</p>
+        <p>Due Date: {customer.dueDate}</p>
+        <p>Period: {customer.period}</p>
+        <p>Co-Maker: {customer.coMaker}</p>
+      <button>Edit</button>
+      <button>Delete</button>
     </div>
   )
 }
--- src/client/components/CustomerCard.jsx
+++ src/client/components/CustomerCard.jsx
@@ -4,14 +4,12 @@ import { Link } from 'react-router-dom'
 export default function CustomerCard (props) {
 
   const { customer } = props
-  
+
   return (
     <div>
       <Link to={`/customers/${customer.id}`}>
         <p>{customer.familyName}, {customer.givenName} {customer.middleName}</p>
       </Link>
-      <button>Edit</button>
-      <button>Delete</button>
     </div>
   )
   
