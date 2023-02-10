c7ebcfe627bdbdc74b635bee121a69df2d4b4526
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-10 18:04:15 +1300

    add template data for performing CRUD and for showing data on component

---
+++ src/data/customers.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    familyName: 'Roble',
+    givenName: 'Gina',
+    middleName: 'Toledo',
+    address: 'Cambanay, Danao, Cebu',
+    mode: 'daily',
+    balance: '11,500',
+    dueDate: '2023-10-31',
+    period: '15-30',
+    coMaker: 'Lisa Lim'
+  },
+  {
+    id: 2,
+    familyName: 'Montesclaros',
+    givenName: 'Anne',
+    middleName: 'Gomez',
+    address: 'Carmen, Cebu',
+    mode: 'weekly',
+    balance: '20,800',
+    dueDate: '2023-11-15',
+    period: '15-30',
+    coMaker: 'Rhoda Oporto'
+  },
+  {
+    id: 3,
+    familyName: 'Tano',
+    givenName: 'Emily',
+    middleName: 'Pasaje',
+    address: 'Pio del Pilar, Danao City',
+    mode: 'semi-monthly',
+    balance: '5,600',
+    dueDate: '2023-06-18',
+    period: '05-20',
+    coMaker: 'Neri Tampus'
+  },
+  {
+    id: 4,
+    familyName: 'Ceniza',
+    givenName: 'Christina',
+    middleName: 'Lao',
+    address: 'Looc, Danao City',
+    mode: 'semi-monthly',
+    balance: '5,600',
+    dueDate: '2023-06-18',
+    period: '05-20',
+    coMaker: 'Neri Tampus'
+  }
+]
