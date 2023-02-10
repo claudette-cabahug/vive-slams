4f8dc53f1bb6aaa3c2af46132d945c3fbb9f8f6d
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-10 19:09:01 +1300

    map the customers and pass props to child component CustomerCard

--- src/client/components/Customers.jsx
+++ src/client/components/Customers.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
+import customers from '../../data/customers'
+
+import CustomerCard from './CustomerCard'
+
 export default class Customers extends Component {
   render() {
     return (
       <div>
-        <h1>Customers</h1>
+        <h1>All Customers</h1>
+        <div>
+          {customers.map(customer => {
+            return <CustomerCard key={customer.id} customer={customer} />
+          })}
+        </div>
       </div>
     )
   }
