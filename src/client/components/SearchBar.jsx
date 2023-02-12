24d952b36580c1a5ed347adef9cc87bcb1b9f130
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-12 17:59:31 +1300

    added functionality to search bar add a handleChange function to reflect what user type on search bar useState to add the typed data into state filter the list of customers map the results to show the full name of customer use TextField library to style the search bar add a link to the individual page of the customer from search result

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,22 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import TextField from "@mui/material/TextField"
 
-export default class SearchBar extends Component {
+import customers from '../../data/customers'
 
-  render() {
-    return (
-    <div className="main">
-      <h1>Search</h1>
-      <div className="search">
+export default function SearchBar () {
+
+  //const [query, setQuery] = useState('')
+
+  const [state, setState] = useState({
+    query: '',
+    list: []
+  })
+
+  const handleChange = (e) => {
+    const results = customers.filter( customer => {
+      if ( e.target.value === "" ) return customers
+      return customer.familyName.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    setState({
+     query: e.target.value,
+     list: results 
+    })
+  }
+
+  return (
+    <div>
+      <div>
         <TextField
           id="outlined-basic"
           variant="outlined"
           fullWidth
           label="Search"
+
+          value={state.query} 
+          onChange={handleChange} 
         />
+        <ul>
+          {
+            (state.query === '' ? "" : !state.list.length ? "No customer match the query" : state.list.map( customer => {
+              return (
+              <>
+                <Link to={`/customers/${customer.id}`}>
+                  <li key={customer.id}> {customer.familyName}, {customer.givenName} </li>
+                </Link>
+              </>
+              )
+            }))
+          }
+        </ul>
       </div>
-      {/* <List /> */}
     </div>
-    )
-  }
+  )
 }
+
+// resource: https://www.makeuseof.com/react-search-bar-filters-data-create/ 
