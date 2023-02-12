1a44524cf3586e2d9f87c63ffdc94e871e7144d1
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-02-12 15:30:02 +1300

    add photo to homepage with basic styling

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,11 +1,17 @@
+import { flexbox } from "@mui/system";
 import React from "react";
+import photo from "../../server/public/images/homepage.jpg"
 
 export default function Home() {
   return (
     <div>
       <h1>VIVE</h1>
       <h3>A Simple Loan Application & Management System</h3>
-      {/* insert image here */}
+      <img 
+        src={photo} 
+        alt="a person recording accounting data on ledger with accounting tools like calculator on the table" 
+        style={{ height: 400, display: flexbox, flexDirection: "column", alignSelf: "center", alignItems: "center", justifyContent: "center" }} 
+      />
     </div>
   );
 }
